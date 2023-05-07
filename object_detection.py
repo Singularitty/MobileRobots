a3a120742ce19059a3e1d91e3e485da6f9838312
Singularitty <luispedroferreirinha@gmail.com>
2023-05-07 21:22:08 +0100

    Object Detection Integration

--- object_detection.py
+++ object_detection.py
@@ -1,14 +1,12 @@
 import socket
 import sys
-import os
-import datetime
 import signal
 from time import sleep
-from matplotlib import category
+from threading import Lock
 
 from pyparsing import line
 
-HOST = "192.168.43.236"  # The server's hostname or IP address -> "Default Gateway" of hotspot
+HOST = "192.168.30.217"  # The server's hostname or IP address -> "Default Gateway" of hotspot
 PORT = 8888
 
 soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -26,12 +24,8 @@ class Buffer:
                 return None
             self.buffer += data
         line,sep,self.buffer = self.buffer.partition(b':')
-        print(line.decode())
+        #print(line.decode())
         return line.decode()
-
-def interrupt_handler(signal, frame):
-    print("Stopping data acquisition")
-    sys.exit(0)
 
 def connect():
     while(1):
@@ -45,8 +39,27 @@ def connect():
             print(e)
             sleep(1)
 
-def main():
-    print("start")
+
+class detection:
+    
+    def __init__(self,object_number, picture_dimensions, obj_dimensions, relative_positions, categories_number, categories):
+        self.object_number = object_number
+        self.picture_dimensions = picture_dimensions
+        self.obj_dimensions = obj_dimensions
+        self.relative_positions = relative_positions
+        self.categories_number = categories_number
+        self.categories = categories
+        
+    def get_position_of_object(self, target_category):
+        if self.categories is not None or len(self.categories) > 0:
+            for i, (category, _) in enumerate(self.categories):
+                if category == target_category:
+                    relative_x, relative_y = self.relative_positions[i]
+                    x = float(self.picture_dimensions[0]) * float(relative_x)
+                    return x
+        return None
+
+def detect_objects(shared_mem: list[detection], mutex: Lock):
     
     while(1):
         soc = connect()
@@ -55,7 +68,7 @@ def main():
         while True:
             b = Buffer(soc)
             start = b.get_line()
-            print("Start", start)
+            #print("Start", start)
             
             try:
                 if(int(start) == 12):
@@ -75,14 +88,24 @@ def main():
                        # for j in range(categories_number):
                         categories.append([b.get_line(), b.get_line()])
 
-                        relative_position.append([b.get_line(),b.get_line()])                    
+                        relative_position.append([b.get_line(),b.get_line()])        
+                        
+                    objs = detection(object_number, picture_dimensions, obj_dimensions, relative_position, categories_number, categories)            
                 
-                    print("Number of objects:", object_number)
-                    print("Picture Dimensions", picture_dimensions)
-                    print("Object Dimensions",obj_dimensions)
-                    print("Relative dimensions", relative_position)
-                    print("Number of categories:", categories_number)
-                    print(categories)
+                    #print("Number of objects:", object_number)
+                    #print("Picture Dimensions", picture_dimensions)
+                    #print("Object Dimensions",obj_dimensions)
+                    #print("Relative dimensions", relative_position)
+                    #print("Number of categories:", categories_number)
+                    #print(categories)
+                    
+                    #print(objs.get_position_of_object("bottle"))
+                    
+                    if not mutex.locked():
+                        mutex.acquire()
+                        shared_mem[0] = objs
+                        mutex.release()
+                    
             except Exception as ex:
                 try: 
                     soc.close()
@@ -91,9 +114,3 @@ def main():
                 print(ex)
                 print("Connection error")
                 break
-                    
-   
-
-if __name__ == "__main__":
-    signal.signal(signal.SIGINT, interrupt_handler)
-    main()
